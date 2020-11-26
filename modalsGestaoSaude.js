44ccd09171df8639e2f0065ebc73797ef62ca16e
gleysson-unimed <74430110+gleysson-unimed@users.noreply.github.com>
2020-11-26 09:33:45 -0300

    Add files via upload

---
+++ modalsGestaoSaude.js
@@ -0,0 +1,140 @@
+function modalBebes () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalCriancas () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalAdolescentes () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalAdultos () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalIdosos () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalGestantes () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalEspeciais () {
+var myModal = FLUIGC.modal({
+    title: 'Title',
+    content: '<h1>Modal Content</h1>',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Save',
+        'bind': 'data-open-modal',
+    },{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
