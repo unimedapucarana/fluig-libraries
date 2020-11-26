af6f3ef998430082387d7c6f3cfa201326f68b12
gleysson-unimed <74430110+gleysson-unimed@users.noreply.github.com>
2020-11-26 11:05:20 -0300

    Add files via upload

---
+++ modalsGestaoSaudeV2.js
@@ -0,0 +1,119 @@
+function modalBebes () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Bebês',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14218&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalCriancas () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Crianças',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14222&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalAdolescentes () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Adolescentes',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14217&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalAdultos () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Adultos',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14219&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalIdosos () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Idosos',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14223&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalGestantes () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Gestantes',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14220&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
+function modalEspeciais () {
+var myModal = FLUIGC.modal({
+    title: 'Saúde Bucal: Situações Especiais',
+    content: '<img src="/webdesk/streamcontrol/?WDCompanyId=1&amp;WDNrDocto=14221&amp;WDNrVersao=1000" alt="Saúde Bucal" width="535" height="1008">',
+    id: 'fluig-modal',
+    actions: [{
+        'label': 'Close',
+        'autoClose': true
+    }]
+}, function(err, data) {
+    if(err) {
+        // do error handling
+    } else {
+        // do something with data
+    }
+});
+}
